36f128558e841f226590c513f0206bfdb4fb18ef
GPW <GPW@BST-R11>
2021-08-06 16:40:27 +0530

    Implement app to app for android

--- Dstricts.Android/MainActivity.cs
+++ Dstricts.Android/MainActivity.cs
@@ -1,17 +1,23 @@
 ﻿using Android.OS;
 using Android.App;
 using Android.Runtime;
+using Android.Content;
 using Android.Content.PM;
 
 namespace Dstricts.Droid
 {
 	[Activity(Label = "Dstricts", Icon = "@drawable/appIcon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+	[IntentFilter(new[] { Intent.ActionView },
+				  DataScheme = "https",
+				  DataHost = "Dstricts.com",
+				  DataPathPrefix = "/session",
+				  AutoVerify = true,
+				  Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable })]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
-
 			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
 			global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
 			LoadApplication(new App());
