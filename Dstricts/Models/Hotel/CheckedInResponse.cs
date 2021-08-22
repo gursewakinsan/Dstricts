e76785e3446e4411d5ef566d863922e5ce7fa67c
GPW <GPW@BST-R11>
2021-08-22 22:11:05 +0530

    Checked In done

--- Dstricts/Models/Hotel/CheckedInResponse.cs
+++ Dstricts/Models/Hotel/CheckedInResponse.cs
@@ -2,5 +2,20 @@
 {
 	public class CheckedInResponse
 	{
+		[Newtonsoft.Json.JsonProperty(PropertyName = "id")]
+		public int Id { get; set; }
+
+		[Newtonsoft.Json.JsonProperty(PropertyName = "property_nickname")]
+		public string PropertyNickName { get; set; }
+
+		[Newtonsoft.Json.JsonProperty(PropertyName = "address")]
+		public string Address { get; set; }
+
+		[Newtonsoft.Json.JsonProperty(PropertyName = "enc")]
+		public string Enc { get; set; }
+
+		[Newtonsoft.Json.JsonProperty(PropertyName = "qloud_checkout_id")]
+		public string QloudCheckOutId { get; set; }
+		public string FirstLetterName => System.Globalization.StringInfo.GetNextTextElement(PropertyNickName, 0).ToUpper();
 	}
 }
