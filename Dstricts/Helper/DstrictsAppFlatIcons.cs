48ccc467322b9f2bad6f1d0fcad67236c3182a4c
GPW <GPW@BST-R11>
2021-08-25 14:04:07 +0530

    Implement bottom bar and find icons as per requirement

--- Dstricts/Helper/DstrictsAppFlatIcons.cs
+++ Dstricts/Helper/DstrictsAppFlatIcons.cs
@@ -14,5 +14,10 @@
 		public const string Heart = "\U000f02d1";
 		public const string Chat = "\U000f0b79";
 		public const string StarOutline = "\U000f04d2";
+		public const string BellOutline = "\U000f009c";
+		public const string Account = "\U000f0004";
+		public const string HomeCircleOutline = "\U000f104d";
+		public const string SearchIcon = "\U000f0349";
+		public const string ShareOutline = "\U000f0932";
 	}
 }
