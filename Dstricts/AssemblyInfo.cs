549371e2cf4fafbf98cac43c4666aff2cd145c1b
GPW <GPW@BST-R11>
2021-08-31 22:37:33 +0530

    Implement font family with new way

--- Dstricts/AssemblyInfo.cs
+++ Dstricts/AssemblyInfo.cs
@@ -2,4 +2,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("SansProBold.ttf", Alias = "SansProBoldFontFamily")]
+[assembly: ExportFont("SansProRegular.ttf", Alias = "SansProRegularFontFamily")]
+[assembly: ExportFont("SansProSemiBold.ttf", Alias = "SansProSemiBoldFontFamily")]
 [assembly: ExportFont("SourceSansProBlack.ttf", Alias = "SourceSansProBlackFontFamily")]
