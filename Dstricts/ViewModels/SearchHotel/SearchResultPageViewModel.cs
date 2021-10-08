2c4805e3557293ad3470bc8938cad2823f62882b
GPW <GPW@BST-R11>
2021-10-08 15:07:55 +0530

    Implement SearchResultPage

---
+++ Dstricts/ViewModels/SearchHotel/SearchResultPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Xamarin.Forms;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Dstricts.ViewModels
+{
+	public class SearchResultPageViewModel : BaseViewModel
+	{
+		#region Constructor.
+		public SearchResultPageViewModel(INavigation navigation)
+		{
+			Navigation = navigation;
+			SearchInfoList = new ObservableCollection<SearchInfo>();
+			for (int i = 0; i < 10; i++)
+			{
+				SearchInfoList.Add(new SearchInfo()
+				{
+					Url= "https://www.pngtosvg.com/wp-content/uploads/2018/08/pngtosvghome.jpg",
+					Name = $"Name {i}",
+					Details = $"Details {i}"
+				});
+			}
+		}
+		#endregion
+
+		#region Search Result Command.
+		private ICommand searchResultCommand;
+		public ICommand SearchResultCommand
+		{
+			get => searchResultCommand ?? (searchResultCommand = new Command(async () => await ExecuteSearchResultCommand()));
+		}
+		private async Task ExecuteSearchResultCommand()
+		{
+			await Task.CompletedTask;
+		}
+		#endregion
+
+		#region Properties.
+		public ObservableCollection<SearchInfo> SearchInfoList { get; set; }
+		#endregion
+	}
+}
+
+public class SearchInfo
+{
+	public string Url { get; set; }
+	public string Name { get; set; }
+	public string Details { get; set; }
+}
---
+++ Dstricts/Views/SearchHotel/SearchResultPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Dstricts.ViewModels;
+
+namespace Dstricts.Views.SearchHotel
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SearchResultPage : ContentPage
+	{
+		SearchResultPageViewModel viewModel;
+		public SearchResultPage()
+		{
+			InitializeComponent();
+			NavigationPage.SetBackButtonTitle(this, "");
+			BindingContext = viewModel = new SearchResultPageViewModel(this.Navigation);
+		}
+
+		private void OnSearchResultItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			listSearchResult.SelectedItem = null;
+		}
+	}
+}
