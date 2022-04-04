de13f723e74bca39986e9d0db6d42b7a9af715af
GPW <GPW@BST-R11>
2022-04-04 22:43:02 +0530

    fixes

--- Dstricts/ViewModels/Hotel/CheckInPageViewModel.cs
+++ Dstricts/ViewModels/Hotel/CheckInPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 using Dstricts.Service;
 using Dstricts.Interfaces;
 using System.Windows.Input;
@@ -7,8 +8,8 @@ using System.Collections.Generic;
 
 namespace Dstricts.ViewModels
 {
-    public class CheckInPageViewModel : BaseViewModel
-    {
+	public class CheckInPageViewModel : BaseViewModel
+	{
 		#region Constructor.
 		public CheckInPageViewModel(INavigation navigation)
 		{
@@ -32,9 +33,9 @@ namespace Dstricts.ViewModels
 				UserId = Helper.Helper.LoggedInUserId
 			});
 
-			responseCheckedIn.Add(new Models.CheckedInResponse() { PropertyNickName = "Hotel 1" });
+			/*responseCheckedIn.Add(new Models.CheckedInResponse() { PropertyNickName = "Hotel 1" });
 			responseCheckedIn.Add(new Models.CheckedInResponse() { PropertyNickName = "Hotel 2" });
-			responseCheckedIn.Add(new Models.CheckedInResponse() { PropertyNickName = "Hotel 3" });
+			responseCheckedIn.Add(new Models.CheckedInResponse() { PropertyNickName = "Hotel 3" });*/
 
 			var responseCheckedInApartment = await service.CheckedInApartmentAsync(new Models.CheckedInRequest()
 			{
@@ -45,10 +46,10 @@ namespace Dstricts.ViewModels
 			{
 				UserId = Helper.Helper.LoggedInUserId
 			});
-			responseCheckedInMeetingList.Add(new Models.CheckedInMeetingListResponse() {Name = "Name 1",CompanyName= "CompanyName 1" });
+			/*responseCheckedInMeetingList.Add(new Models.CheckedInMeetingListResponse() { Name = "Name 1", CompanyName = "CompanyName 1" });
 			responseCheckedInMeetingList.Add(new Models.CheckedInMeetingListResponse() { Name = "Name 2", CompanyName = "CompanyName 2" });
 			responseCheckedInMeetingList.Add(new Models.CheckedInMeetingListResponse() { Name = "Name 3", CompanyName = "CompanyName 3" });
-			responseCheckedInMeetingList.Add(new Models.CheckedInMeetingListResponse() { Name = "Name 4", CompanyName = "CompanyName 4" });
+			responseCheckedInMeetingList.Add(new Models.CheckedInMeetingListResponse() { Name = "Name 4", CompanyName = "CompanyName 4" });*/
 			CheckedInMeetingList = responseCheckedInMeetingList;
 
 			if (responseCheckedIn?.Count > 0)
@@ -92,6 +93,150 @@ namespace Dstricts.ViewModels
 		}
 		#endregion
 
+		#region Verify QR Code Command.
+		private ICommand verifyQrCodeCommand;
+		public ICommand VerifyQrCodeCommand
+		{
+			get => verifyQrCodeCommand ?? (verifyQrCodeCommand = new Command<string>(async (qrCode) => await ExecuteVerifyQrCodeCommand(qrCode)));
+		}
+		private async Task ExecuteVerifyQrCodeCommand(string qrCode)
+		{
+			VerifyCheckinId = string.Empty;
+			string[] codeInfo = qrCode.Split('/');
+			switch (codeInfo[0])
+			{
+				case "verify_checkin":
+					HotelPropertyType = Convert.ToInt32(codeInfo[1]);
+					if (codeInfo.Length == 3)
+					{
+						VerifyCheckinId = codeInfo[2];
+						VerifyBookingCheckinCommand.Execute(codeInfo[2]);
+					}
+					else
+						GoToVerifyCheckedInCodePageCommand.Execute(null);
+					break;
+				case "getQueue":
+					Helper.Helper.AvalibleQueueId = codeInfo[1];
+					await Navigation.PushAsync(new Views.Queue.AvalibleQueueOnTheLocationPage());
+					break;
+				case "verify_adult_checkin":
+					VerifyUserInvitationInfoCommand.Execute(codeInfo[2]);
+					break;
+				case "verify_visitor":
+					InvitedVisitorsMeetingListCommand.Execute(codeInfo[2]);
+					break;
+			}
+			await Task.CompletedTask;
+		}
+		#endregion
+
+		#region Verify Booking Checkin Command.
+		private ICommand verifyBookingCheckinCommand;
+		public ICommand VerifyBookingCheckinCommand
+		{
+			get => verifyBookingCheckinCommand ?? (verifyBookingCheckinCommand = new Command<string>(async (id) => await ExecuteVerifyBookingCheckinCommand(id)));
+		}
+		private async Task ExecuteVerifyBookingCheckinCommand(string id)
+		{
+			DependencyService.Get<IProgressBar>().Show();
+			IHotelService service = new HotelService();
+			var response = await service.VerifyBookingCheckinAsync(new Models.VerifyBookingCheckinRequest()
+			{
+				Id = id,
+				DstrictsUserId = Helper.Helper.LoggedInUserId
+			});
+			if (response == 0)
+				await Navigation.PushAsync(new Views.ErrorMessage.BookingNotAvailablePage());
+			else if (response == 1)
+				GoToVerifyCheckedInCodePageCommand.Execute(null);
+			else if (response == 2)
+				await Navigation.PushAsync(new Views.ErrorMessage.FrontDeskPage());
+			else if (response == 3)
+				await Navigation.PushAsync(new Views.ErrorMessage.AlreadyCheckedInForHotelPage());
+			//In verify_checkin array 3 hai to api call krni hai
+			//TODO https://dstricts.com/user/index.php/DstrictsApp/verifyBookingCheckin
+			//id,jo 3 arrary ki value hai and  dstricts_user_id
+			//after call api if return 0 then navigate to booking not avilable
+			// if 1 then current flow same as
+			// if 3 then naviagte to already checked in page
+			//if 2 then new page "Front desk"
+			DependencyService.Get<IProgressBar>().Hide();
+		}
+		#endregion
+
+		#region Go To Verify Checked In Code Page Command.
+		private ICommand goToVerifyCheckedInCodePageCommand;
+		public ICommand GoToVerifyCheckedInCodePageCommand
+		{
+			get => goToVerifyCheckedInCodePageCommand ?? (goToVerifyCheckedInCodePageCommand = new Command(async () => await ExecuteGoToVerifyCheckedInCodePageCommand()));
+		}
+		private async Task ExecuteGoToVerifyCheckedInCodePageCommand()
+		{
+			await Navigation.PushAsync(new Views.Hotel.VerifyCheckedInPage(HotelPropertyType, VerifyCheckinId));
+		}
+		#endregion
+
+		#region Verify User Invitation Info Command.
+		private ICommand verifyUserInvitationInfoCommand;
+		public ICommand VerifyUserInvitationInfoCommand
+		{
+			get => verifyUserInvitationInfoCommand ?? (verifyUserInvitationInfoCommand = new Command<string>(async (id) => await ExecuteVerifyUserInvitationInfoCommand(id)));
+		}
+		private async Task ExecuteVerifyUserInvitationInfoCommand(string id)
+		{
+			DependencyService.Get<IProgressBar>().Show();
+			IHotelService hotelService = new HotelService();
+			int code = await hotelService.VerifyUserInvitationInfoAsync(new Models.VerifyUserInvitationInfoRequest()
+			{
+				Id = id,
+				UserId = Helper.Helper.LoggedInUserId
+			});
+			if (code == 0) //Not authorized page
+				Application.Current.MainPage = new NavigationPage(new Views.ErrorMessage.NotAuthorizedForHotelCheckInPage());
+			else if (code == 1) // Already checked-in page
+				Application.Current.MainPage = new NavigationPage(new Views.ErrorMessage.AlreadyCheckedInForHotelPage());
+			else if (code == 2) // Wrong date page
+				Application.Current.MainPage = new NavigationPage(new Views.ErrorMessage.WrongDateForHotelCheckInPage());
+			else
+			{
+				Helper.Helper.VerifyUserInvitationInfoId = id;
+				await Navigation.PushAsync(new Views.Hotel.VerifyUserInvitationInfoCodePage(code));
+			}
+			DependencyService.Get<IProgressBar>().Hide();
+		}
+		#endregion
+
+		#region Invited Visitors Meeting List Command.
+		private ICommand invitedVisitorsMeetingListCommand;
+		public ICommand InvitedVisitorsMeetingListCommand
+		{
+			get => invitedVisitorsMeetingListCommand ?? (invitedVisitorsMeetingListCommand = new Command<string>(async (locationId) => await ExecuteInvitedVisitorsMeetingListCommand(locationId)));
+		}
+		private async Task ExecuteInvitedVisitorsMeetingListCommand(string locationId)
+		{
+			DependencyService.Get<IProgressBar>().Show();
+			IVisitorsService service = new VisitorsService();
+			var response = await service.InvitedVisitorsMeetingListAsync(new Models.InvitedVisitorsMeetingListRequest()
+			{
+				LocationId = locationId,
+				UserId = Helper.Helper.LoggedInUserId,
+			});
+			if (response?.Count > 0)
+			{
+				foreach (var visitors in response)
+				{
+					visitors.IsSelectedVisitors = false;
+					visitors.VisitorsCardBorderColor = Color.FromHex("#363541");
+					visitors.VisitorsNameTextOpacity = 0.4;
+				}
+				await Navigation.PushAsync(new Views.Visitors.InvitedVisitorsMeetingListPage(response));
+			}
+			else
+				await Navigation.PushAsync(new Views.ErrorMessage.InvitedVisitorsMeetingListNotAvailablePage());
+			DependencyService.Get<IProgressBar>().Hide();
+		}
+		#endregion
+
 		#region Properties.
 		private List<Models.CheckedInResponse> checkInList;
 		public List<Models.CheckedInResponse> CheckInList
@@ -114,6 +259,9 @@ namespace Dstricts.ViewModels
 				OnPropertyChanged("CheckedInMeetingList");
 			}
 		}
+
+		public int HotelPropertyType { get; set; }
+		public string VerifyCheckinId { get; set; } = string.Empty;
 		#endregion
 	}
 }
--- Dstricts/Views/Hotel/CheckInPage.xaml.cs
+++ Dstricts/Views/Hotel/CheckInPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Dstricts.ViewModels;
+using ZXing.Net.Mobile.Forms;
 using System.Threading.Tasks;
 
 namespace Dstricts.Views.Hotel
@@ -8,13 +10,19 @@ namespace Dstricts.Views.Hotel
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CheckInPage : ContentPage
 	{
+		#region Variables.
 		CheckInPageViewModel viewModel;
+		ZXingScannerPage scanPage;
+		#endregion
+
+		#region Constructor.
 		public CheckInPage ()
 		{
 			InitializeComponent ();
 			NavigationPage.SetBackButtonTitle(this, "");
 			BindingContext = viewModel = new CheckInPageViewModel(this.Navigation);
 		}
+		#endregion
 
 		#region On Appearing.
 		protected override void OnAppearing()
@@ -63,6 +71,52 @@ namespace Dstricts.Views.Hotel
 			}
 			else if (checkedIn.Enc == 2) //Apartment
 				await Navigation.PushAsync(new Apartment.QloudIdApartmentDetailsPage(checkedIn));
+		}
+		#endregion
+
+		#region Scan QR Code.
+		private void OnBackClicked(object sender, EventArgs e)
+		{
+			Device.BeginInvokeOnMainThread(async () =>
+			{
+				this.scanPage.IsScanning = false;
+				await Navigation.PopModalAsync();
+			});
+		}
+
+		private async void OnBtnOpenCameraToScanQrCodeClicked(object sender, EventArgs e)
+		{
+			var customOverlay = new StackLayout
+			{
+				HorizontalOptions = LayoutOptions.StartAndExpand,
+				VerticalOptions = LayoutOptions.StartAndExpand
+			};
+
+			var back = new ImageButton
+			{
+				Margin = new Thickness(15, 20, 0, 0),
+				BackgroundColor = Color.Transparent,
+				Source = "iconBack.png",
+				Padding = 10,
+				HeightRequest = 50,
+				WidthRequest = 50,
+				HorizontalOptions = LayoutOptions.StartAndExpand,
+				VerticalOptions = LayoutOptions.StartAndExpand
+			};
+
+			back.Clicked += OnBackClicked;
+			customOverlay.Children.Add(back);
+
+			this.scanPage = new ZXingScannerPage(customOverlay: customOverlay);
+			scanPage.OnScanResult += (result) => {
+				scanPage.IsScanning = false;
+				Device.BeginInvokeOnMainThread(async () => {
+					await Navigation.PopModalAsync();
+					viewModel.VerifyQrCodeCommand.Execute(result.Text);
+				});
+			};
+			scanPage.IsScanning = true;
+			await Navigation.PushModalAsync(scanPage);
 		}
 		#endregion
 	}
