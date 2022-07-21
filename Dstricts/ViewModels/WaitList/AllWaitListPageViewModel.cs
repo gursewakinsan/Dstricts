53f9fc3736aedef4a452f09a91f920cb75bbdd1f
gursewakinsan <insagursewakinsa@gmail.com>
2022-07-21 23:13:47 +0530

    fixs

--- Dstricts/ViewModels/WaitList/AllWaitListPageViewModel.cs
+++ Dstricts/ViewModels/WaitList/AllWaitListPageViewModel.cs
@@ -39,5 +39,29 @@ namespace Dstricts.ViewModels
 			}
 		}
 		#endregion
+
+		#region Socail Click Command .
+		private ICommand socailClickCommand;
+		public ICommand SocailClickCommand
+		{
+			get => socailClickCommand ?? (socailClickCommand = new Command(() => ExecuteSocailClickCommand()));
+		}
+		private void ExecuteSocailClickCommand()
+		{
+			Application.Current.MainPage = new NavigationPage(new Views.Hotel.CheckedInListPage());
+		}
+		#endregion
+
+		#region Go To Check In Command.
+		private ICommand goToCheckInCommand;
+		public ICommand GoToCheckInCommand
+		{
+			get => goToCheckInCommand ?? (goToCheckInCommand = new Command( () =>  ExecuteGoToCheckInCommand()));
+		}
+		private void ExecuteGoToCheckInCommand()
+		{
+			Application.Current.MainPage = new NavigationPage(new Views.Hotel.CheckInPage());
+		}
+		#endregion
 	}
 }
