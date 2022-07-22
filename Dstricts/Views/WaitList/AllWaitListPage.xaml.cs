b17d7801f2b2d6e3ec2db094af1306e8bae1f2c4
apple <gursewaksinghs@gmail.com>
2022-07-22 08:46:04 +0530

    fixs

--- Dstricts/Views/WaitList/AllWaitListPage.xaml.cs
+++ Dstricts/Views/WaitList/AllWaitListPage.xaml.cs
@@ -2,6 +2,7 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Dstricts.ViewModels;
+using ZXing.Net.Mobile.Forms;
 using System.Collections.Generic;
 
 namespace Dstricts.Views.WaitList
@@ -9,7 +10,11 @@ namespace Dstricts.Views.WaitList
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class AllWaitListPage : ContentPage
 	{
+		#region Variables.
 		AllWaitListPageViewModel viewModel;
+		ZXingScannerPage scanPage;
+		#endregion
+
 		public AllWaitListPage(List<Models.UserQueueListResponse> allWaitList)
 		{
 			InitializeComponent();
@@ -81,5 +86,51 @@ namespace Dstricts.Views.WaitList
 			else
 				await Navigation.PushAsync(new AllWaitListDetailInServicingPage(selectedWaitList));
 		}
+
+		#region Scan QR Code.
+		private void OnBackClicked(object sender, EventArgs e)
+		{
+			Device.BeginInvokeOnMainThread(async () =>
+			{
+				this.scanPage.IsScanning = false;
+				await Navigation.PopModalAsync();
+			});
+		}
+
+		private async void OnBtnOpenCameraToScanQrCodeClicked(object sender, EventArgs e)
+		{
+			var customOverlay = new StackLayout
+			{
+				HorizontalOptions = LayoutOptions.StartAndExpand,
+				VerticalOptions = LayoutOptions.StartAndExpand
+			};
+
+			var back = new ImageButton
+			{
+				Margin = new Thickness(15, 20, 0, 0),
+				BackgroundColor = Color.Transparent,
+				Source = "iconBack.png",
+				Padding = 10,
+				HeightRequest = 50,
+				WidthRequest = 50,
+				HorizontalOptions = LayoutOptions.StartAndExpand,
+				VerticalOptions = LayoutOptions.StartAndExpand
+			};
+
+			back.Clicked += OnBackClicked;
+			customOverlay.Children.Add(back);
+
+			this.scanPage = new ZXingScannerPage(customOverlay: customOverlay);
+			scanPage.OnScanResult += (result) => {
+				scanPage.IsScanning = false;
+				Device.BeginInvokeOnMainThread(async () => {
+					await Navigation.PopModalAsync();
+					viewModel.VerifyQrCodeCommand.Execute(result.Text);
+				});
+			};
+			scanPage.IsScanning = true;
+			await Navigation.PushModalAsync(scanPage);
+		}
+		#endregion
 	}
 }
