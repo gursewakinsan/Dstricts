518c70a2af267481da771709c7bc531a0181eca9
gursewakinsan <insagursewakinsa@gmail.com>
2022-08-24 08:44:45 +0530

    design changes on my page info

--- Dstricts/ViewModels/Apartment/MyPageInfoViewModel.cs
+++ Dstricts/ViewModels/Apartment/MyPageInfoViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Xamarin.Forms;
+using System.Windows.Input;
+using System.Threading.Tasks;
 
 namespace Dstricts.ViewModels
 {
@@ -8,6 +10,55 @@ namespace Dstricts.ViewModels
 		public MyPageInfoViewModel(INavigation navigation)
 		{
 			Navigation = navigation;
+		}
+		#endregion
+
+		#region Socail Click Command.
+		private ICommand socailClickCommand;
+		public ICommand SocailClickCommand
+		{
+			get => socailClickCommand ?? (socailClickCommand = new Command(() => ExecuteSocailClickCommand()));
+		}
+		private void ExecuteSocailClickCommand()
+		{
+			Application.Current.MainPage = new NavigationPage(new Views.Hotel.CheckedInListPage());
+		}
+		#endregion
+
+		#region Go To Community Page Command.
+		private ICommand goToCommunityPageCommand;
+		public ICommand GoToCommunityPageCommand
+		{
+			get => goToCommunityPageCommand ?? (goToCommunityPageCommand = new Command(async () => await ExecuteGoToCommunityPageCommand()));
+		}
+		private async Task ExecuteGoToCommunityPageCommand()
+		{
+			if (Helper.Helper.CommunityId > 0)
+				await Navigation.PushAsync(new Views.Community.CommunityPage());
+		}
+		#endregion
+
+		#region Go To Support Page Command.
+		private ICommand goToSupportPageCommand;
+		public ICommand GoToSupportPageCommand
+		{
+			get => goToSupportPageCommand ?? (goToSupportPageCommand = new Command(async () => await ExecuteGoToSupportPageCommand()));
+		}
+		private async Task ExecuteGoToSupportPageCommand()
+		{
+			await Navigation.PushAsync(new Views.Apartment.SupportPage());
+		}
+		#endregion
+
+		#region Go To Apartment Page Command. 
+		private ICommand goToApartmentPageCommand;
+		public ICommand GoToApartmentPageCommand
+		{
+			get => goToApartmentPageCommand ?? (goToApartmentPageCommand = new Command(() => ExecuteGoToApartmentPageCommand()));
+		}
+		private void ExecuteGoToApartmentPageCommand()
+		{
+			Application.Current.MainPage = new NavigationPage(new Views.Apartment.QloudIdApartmentDetailsPage(Helper.Helper.ApartmentCheckedIn));
 		}
 		#endregion
 
