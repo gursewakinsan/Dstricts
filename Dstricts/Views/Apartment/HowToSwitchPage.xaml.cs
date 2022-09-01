d9845fb721e27f19ed064eaf82bebf80935f56fb
gursewakinsan <insagursewakinsa@gmail.com>
2022-09-01 23:46:18 +0530

    working on it...

---
+++ Dstricts/Views/Apartment/HowToSwitchPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Dstricts.ViewModels;
+
+namespace Dstricts.Views.Apartment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HowToSwitchPage : ContentPage
+    {
+        HowToSwitchPageViewModel viewModel;
+        public HowToSwitchPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "");
+            BindingContext = viewModel = new HowToSwitchPageViewModel(this.Navigation);
+        }
+    }
+}
