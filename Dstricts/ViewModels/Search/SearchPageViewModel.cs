22ce71da6ec987e08099937eef5defb7d45305ab
gursewakwavelabs <gursewak.singh@wavelabs.ai>
2023-05-20 15:15:11 +0530

    fics

--- Dstricts/ViewModels/Search/SearchPageViewModel.cs
+++ Dstricts/ViewModels/Search/SearchPageViewModel.cs
@@ -377,6 +377,58 @@ namespace Dstricts.ViewModels
         }
         #endregion
 
+        #region Search Hotel By User Command.
+        private ICommand searchHotelByUserCommand;
+        public ICommand SearchHotelByUserCommand
+        {
+            get => searchHotelByUserCommand ?? (searchHotelByUserCommand = new Command(async () => await ExecuteSearchHotelByUserCommand()));
+        }
+        private async Task ExecuteSearchHotelByUserCommand()
+        {
+            Helper.Helper.SelectSearchType = 1;
+            await Navigation.PushAsync(new Views.SearchHotel.SearchWithHintsPage());
+        }
+        #endregion
+
+        #region Search Hotel By Company Command.
+        private ICommand searchHotelByCompanyCommand;
+        public ICommand SearchHotelByCompanyCommand
+        {
+            get => searchHotelByCompanyCommand ?? (searchHotelByCompanyCommand = new Command(async () => await ExecuteSearchHotelByCompanyCommand()));
+        }
+        private async Task ExecuteSearchHotelByCompanyCommand()
+        {
+            Helper.Helper.SelectSearchType = 2;
+            await Navigation.PushAsync(new Views.SearchHotel.SearchWithHintsPage());
+        }
+        #endregion
+
+        #region Search Hotel By Eat & Drink Command.
+        private ICommand searchHotelByEatAndDrinkCommand;
+        public ICommand SearchHotelByEatAndDrinkCommand
+        {
+            get => searchHotelByEatAndDrinkCommand ?? (searchHotelByEatAndDrinkCommand = new Command(async () => await ExecuteSearchHotelByEatAndDrinkCommand()));
+        }
+        private async Task ExecuteSearchHotelByEatAndDrinkCommand()
+        {
+            Helper.Helper.SelectSearchType = 3;
+            await Navigation.PushAsync(new Views.SearchHotel.SearchWithHintsPage());
+        }
+        #endregion
+
+        #region Search Hotel By Wellness Command.
+        private ICommand searchHotelByWellnessCommand;
+        public ICommand SearchHotelByWellnessCommand
+        {
+            get => searchHotelByWellnessCommand ?? (searchHotelByWellnessCommand = new Command(async () => await ExecuteSearchHotelByWellnessCommand()));
+        }
+        private async Task ExecuteSearchHotelByWellnessCommand()
+        {
+            Helper.Helper.SelectSearchType = 4;
+            await Navigation.PushAsync(new Views.SearchHotel.SearchWithHintsPage());
+        }
+        #endregion
+
         #region Properties.
         public List<Models.UserQueueListResponse> UserQueueList { get; set; }
         public int HotelPropertyType { get; set; }
