833d9c026138a62a15d51f7d6cfcec2be6e46020
gursewakinsan <insagursewakinsa@gmail.com>
2023-10-05 19:08:28 +0530

    done in android

--- Dstricts/Views/LoginPage.xaml.cs
+++ Dstricts/Views/LoginPage.xaml.cs
@@ -17,7 +17,7 @@ namespace Dstricts.Views
 
 		protected override void OnAppearing()
 		{
-            Helper.Helper.SessionId = "eGE1c0Q2S1BrSU9naUJwWFJlemZPa0lBdGNEV25Tem91a0Jmdm1HeXQvRT0=";
+			Helper.Helper.SessionId = "bVJzMUkvRWhON3dnR2hTMEdwbGhBZz09"; //"eGE1c0Q2S1BrSU9naUJwWFJlemZPa0lBdGNEV25Tem91a0Jmdm1HeXQvRT0=";
             Helper.Helper.DeviceWidth = Application.Current.MainPage.Width;
 			if (!string.IsNullOrWhiteSpace(Helper.Helper.SessionId))
 				loginViewModel.LoginWithSessionCommand.Execute(null);
